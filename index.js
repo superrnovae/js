0da4f5b614433631b1a9a74e521231d10e6a3f89
superrnovae <vladislavp@pm.me>
2022-12-21 12:32:07 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const scanf = require('scanf');
+
+// Exo 1
+
+console.log('Saisir première chaîne');
+const first = scanf("%s");
+
+console.log('Saisir deuxième chaîne');
+const second = scanf("%s");
+
+if(first === null || second === null) {
+    console.log("Au moins une chaine est vide");
+}
+else if(first.length / second.length >= 2) {
+    console.log("La première chaine est deux fois plus grande que la deuxième");
+}
+else {
+    console.log("La première chaine n'est pas deux fois plus grande que la deuxième");
+}
+
+console.log("");
+
+// Exo 2
+
+console.log('Saisir un entier');
+const entier = scanf("%d");
+
+if (entier === null) {
+    console.log("La valeur saisie est null");
+}
+else {
+    const sum = (entier * (entier+1)) / 2;
+    console.log(`La somme de nombre entre 1 et ${entier} === ${sum}`);
+}
+
+console.log("");
+
+// Exo 3
+
+const arr = [];
+
+for(let i=0; i<5; i++) {
+    console.log(`Saisir entier numero ${i}`);
+    arr.push(scanf("%d"));
+}
+
+console.log(`L'index de plus grand element = ${indexOfMax(arr)}`);
+
+const average = arr.reduce((a, b) => a + b, 0) / arr.length;
+
+console.log(`La moyenne des entiers est ${average}`);
+
+function indexOfMax(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    let max = arr[0];
+    let maxIndex = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+
+    return maxIndex;
+}
+
+
+
